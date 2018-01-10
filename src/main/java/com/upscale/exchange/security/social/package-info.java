55474b02c2344c661cccc9f330110c93fa35924e
maektwain <saranshisat@icloud.com>
2018-01-10 14:05:00 +0530

    Empty Glass or No Glass

--- src/main/java/com/upscale/exchange/security/social/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring social configuration.
- */
-package com.upscale.exchange.security.social;
--- src/main/java/com/upscale/exchange/web/websocket/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Access Objects used by WebSocket services.
- */
-package com.upscale.exchange.web.websocket.dto;
